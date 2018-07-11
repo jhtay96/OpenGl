f0eb54359f8ba9adddeec723c0cf6898a0f47e49
TAY JAN HONG <janhong.tay@sd.taylors.edu.my>
2018-07-11 14:17:02 +0800

    latest

--- OpenGl/main.cpp
+++ OpenGl/main.cpp
@@ -7,6 +7,20 @@
 
 const GLuint WIDTH = 4000, HEIGHT = 4000;
 
+
+
+void displayTime() {
+	glColor4f(0.16, 0.41, 0.17, 0.1);
+	glBegin(GL_POLYGON); {
+		glVertex2f(0.1875, 0.958333333);
+		glVertex2f(-0.1875, 0.958333333);
+		glVertex2f(-0.1875, 0.791666667);
+		glVertex2f(0.1875, 0.791666667);
+	}
+	glEnd();
+}
+
+
 void Grass1(float x,float y) {
 	glColor3f(0.16, 0.41, 0.17);
 	glBegin(GL_POLYGON);
@@ -40,11 +54,16 @@ void Grass1(float x,float y) {
 
 
 
+
+
 void proceessInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
+
+
+
 
 void TopGrass() {
 	glColor3f(0.0, 0.89803922, 0.0);
@@ -57,6 +76,22 @@ void TopGrass() {
 	}
 	glEnd();
 }
+
+
+void TopPlace() {
+	glColor3f(0.0, 0.17647059, 0.0);
+	glBegin(GL_POLYGON);
+	{
+		glVertex2f(0.0, 0.2499999999);
+		glVertex2f(0.9375, -0.3333333);
+		glVertex2f(0.9375, 1);
+		glVertex2f(-0.9375, 1);
+		glVertex2f(-0.9375, -0.3333333);
+	}
+	glEnd();
+
+}
+
 
 
 void TopBorder() {
@@ -114,7 +149,8 @@ int main() {
 
 
 	while (!glfwWindowShouldClose(window)) {
-
+		
+		
 		proceessInput(window);
 		glPointSize(10);
 		glColor3f(1, 0, 0);
@@ -125,7 +161,10 @@ int main() {
 
 		float delta = glfwGetTime();
 
-		
+		glEnable(GL_BLEND);
+		//TopPlace();
+		//displayTime();
+
 		
 		TopGrass();
 		bombField();
@@ -140,6 +179,9 @@ int main() {
 		Grass1(0.1875, -0.5);
 		Grass1(-0.1875, -0.5);
 		Grass1(0, -0.625);
+
+		
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
